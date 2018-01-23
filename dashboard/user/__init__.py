2e23c5baf2c26b6d79b74c161be2fe583adbac2d
guaiwolo <admin@guaiwolou.com>
2018-01-23 18:58:42 +0800

    2.11

--- dashboard/user/__init__.py
+++ dashboard/user/__init__.py
@@ -3,17 +3,25 @@ from django.views.generic import TemplateView, View
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.http import JsonResponse
-
+from django.core.paginator import Paginator, EmptyPage
 
 class UserListView(TemplateView):
     template_name = "user/userlist.html"  #template name
 
     def get_context_data(self, **kwargs):
         context = super(UserListView, self).get_context_data(**kwargs)
-        context['userlist'] = User.objects.all()
+        userlist = User.objects.all()    #获取所有的用户列表对象
+        paginator = Paginator(userlist, 10)   #实例化Paginator
+        page = self.request.GET.get("page", 1)  #获取当前页码
+        try:
+            page_obj = paginator.page(page)    #获取当前页的数据
+        except EmptyPage:
+            page_obj = paginator.page(1)
+        context['page_obj'] = page_obj
         return context
 
     def get(self, request, *args, **kwargs):
+        self.request = request
         return super(UserListView, self).get(request, *args, **kwargs)
 
 
